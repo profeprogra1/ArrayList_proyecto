a6a9669d8674ace832093e1eb05badccbe773229
ricardo <ricardo@10.34.12.254>
2015-11-24 20:00:40 -0300

    24/11/2015 20:00

--- src/arraylist_proyecto/ArrayList_proyecto.java
+++ src/arraylist_proyecto/ArrayList_proyecto.java
@@ -13,9 +13,13 @@ public class ArrayList_proyecto {
         lista.add("PEDRITO");
         lista.add("CARLITOS");
         //OBTENER CANTIDAD DE ELEMENTOS
-        System.out.println("TOTAL:"+lista.size());
+        //System.out.println("TOTAL:"+lista.size());
         //OBTENER ELEMENTO DE POSICION 1
-        System.out.println("POS 1:"+lista.get(1));
+       // System.out.println("POS 1:"+lista.get(1));
+        for(int i=0;i<lista.size();i++){
+            System.out.println("LISTA"+lista.get(i));
+        }
+        
         
     }
 }

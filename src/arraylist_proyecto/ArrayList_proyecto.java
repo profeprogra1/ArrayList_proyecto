8b914c8ad8370e1e6dbdbe3f912d9a6fb9c7d98d
ricardo <ricardo@10.34.12.254>
2015-11-24 20:10:36 -0300

    24/11/2015 20:10

--- src/arraylist_proyecto/ArrayList_proyecto.java
+++ src/arraylist_proyecto/ArrayList_proyecto.java
@@ -7,27 +7,31 @@ package arraylist_proyecto;
 
 import com.ricardo.negocio.Usuario;
 import java.util.ArrayList;
+
 public class ArrayList_proyecto {
+
     public static void main(String[] args) {
-        ArrayList lista=new ArrayList();
+        ArrayList<Usuario> lista = new ArrayList<Usuario>();
         /*lista.add("JUANITO");
-        lista.add("PEDRITO");
-        lista.add("CARLITOS");*/
+         lista.add("PEDRITO");
+         lista.add("CARLITOS");*/
         //OBTENER CANTIDAD DE ELEMENTOS
         //System.out.println("TOTAL:"+lista.size());
         //OBTENER ELEMENTO DE POSICION 1
-       // System.out.println("POS 1:"+lista.get(1));
-        Usuario u=new Usuario();
+        // System.out.println("POS 1:"+lista.get(1));
+        Usuario u = new Usuario();
         u.setId(1);
         u.setNombre("JUAN");
         u.setApellido("PEREZ");
         lista.add(u);
-        for(int i=0;i<lista.size();i++){
-            System.out.println("LISTA"+lista.get(i));
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println("LISTA" + lista.get(i).getId());
+            System.out.println("LISTA" + lista.get(i).getNombre());
+            System.out.println("LISTA" + lista.get(i).getApellido());
+
         }
         //ELIMINAR ELEMENTO PEDRITO
         //lista.remove(1);
-        
-        
+
     }
 }
